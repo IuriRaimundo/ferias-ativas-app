dee22fd4dd8309248a466cab622581adae20c026
Iúri Raimundo <iuriraimundo03@gmail.com>
2021-07-15 10:14:24 +0100

    adição de imagens

--- assets/images/atividades/index.js
+++ assets/images/atividades/index.js
@@ -1,5 +1,15 @@
 const images = {
-  default: require('./imagem1.jpg'),
+  default: require('./default.jpg'),
+  cineteatro: require('./cineteatro.jpg'),
+  bibilioteca: require('./biblioteca.jpg'),
+  bibilioteca2: require('./biblioteca2.jpg'),
+  ponteDaPedra: require('./ponte-da-pedra.jpg'),
+  ponteDaPedra2: require('./ponte-da-pedra2.jpg'),
+  olhosDeÁgua: require('./olhos-de-água.jpg'),
+  olhosDeÁgua2: require('./olhos-de-água2.jpg'),
+  piscinas: require('./piscinas.jpg'),
+  grutas: require('./grutas.jpg'),
+  praça8deMaio: require('./praça-8-de-maio'),
 };
 
 export default images;
